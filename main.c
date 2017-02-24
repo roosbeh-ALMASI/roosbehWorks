33a8a1753083790997fe4525ec06f9a9c577b056
Roosbeh Almasi <roosbeh@outlook.com>
2017-02-24 22:53:04 +0000

    used more array char and pointer

--- main.c
+++ main.c
@@ -2,48 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-void sumUp(int *arr, int s);
-void swap(int *x, int *y);
+
+void showoff(int argc, char *argv)
+{
+   int i = 0;
+   while(*(argv+i) != '\0')
+    {
+       printf("%c, ", *(argv+i));
+       i++;
+    }
+   
+}
+
+void stor(char *argv);
+
+
+
+
+
+//------------------------------------------
 
 int main(void)
 {
-int i;
-
-int lemon[5]={5, 4, 3, 2, 1};
-sort(lemon, 5);
-
-for(i=0; i<5; i++)
-{
-  printf("%d, ", lemon[i]);
-}
-
-
-}
-
-
-
-//------------------------------------
-void swap(int *x, int *y)
-{
-int temp;
-temp = *x;
-*x = *y;
-*y=temp;
-}
-
-
-void sort(int *arr, int s)
-{
-  
-  int i, j;
-  for(i = 0; i<s-1; i++)
-    {
-       for(j=i+1; j<s; j++)
+  int i;
+  char arr[] ="roosbeh almasi";
+   
+  for(i = 0; i<15; i++)
+     {
+       if(arr[i] == 0x20 || arr[i] == '\0')
          {
-            if(*(arr+i) > *(arr+j))
-             {
-               swap(arr+i, arr+j);
-             }
+          printf("\n");
          }
-    }
+       printf("%x, ", arr[i]);
+     }
+
 }
+
+
