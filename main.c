7541e09c00bb847115374dd3ee79241bf2d8a6de
Roosbeh Almasi <roosbeh@outlook.com>
2017-02-24 22:31:56 +0000

    used array and pointer to sort array

--- main.c
+++ main.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void sumUp(int *arr, int s);
+void swap(int *x, int *y);
 
 int main(void)
 {
+int i;
 
-int arr[5], i, j;
-
+int lemon[5]={5, 4, 3, 2, 1};
+sort(lemon, 5);
 
 for(i=0; i<5; i++)
 {
-printf("please enter your %d item \n", i+1);
-scanf("%d", &arr[i] );
+  printf("%d, ", lemon[i]);
 }
 
-for(i=0; i<5; i++)
-printf("%d item = %d\n", i+1, arr[i]);
+
+}
 
 
-return 0;
+
+//------------------------------------
+void swap(int *x, int *y)
+{
+int temp;
+temp = *x;
+*x = *y;
+*y=temp;
+}
+
+
+void sort(int *arr, int s)
+{
+  
+  int i, j;
+  for(i = 0; i<s-1; i++)
+    {
+       for(j=i+1; j<s; j++)
+         {
+            if(*(arr+i) > *(arr+j))
+             {
+               swap(arr+i, arr+j);
+             }
+         }
+    }
 }
