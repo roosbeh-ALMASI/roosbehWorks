3abd18a5fb806832966c0208f17bb03ba2badfe4
Roosbeh Almasi <roosbeh@outlook.com>
2017-03-15 12:41:19 +0000

    I am getting there with Printing the srtings to the uart derivers

--- main.c
+++ main.c
@@ -6,58 +6,30 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void funcky(unsigned char *buffer, int size);
+
+void roosbeh_puts(const char *str);
 
 int main()
 {
 
-unsigned char arr[10];
 
-arr[0] = 0xA0;
-arr[1] = 0x00;
-arr[2] = 0xB0;
-arr[3] = 0x00;
-arr[4] = 0xF0;
-arr[5] = 0xF0;
-arr[6] = 0x03;
-arr[7] = 0x01;
-arr[8] = 0x00;
-arr[9] = 0x0F;
 
-funcky(arr, 10);
+roosbeh_puts("this is going to be awsome\n");
+
 
 return 0;
 
 }
 
-
-
-void funcky(unsigned char *buffer, int size)
+void roosbeh_puts(const char *str)
 {
-
-int j = 0;
-int i = 0;
-int sum = 0;
-uint8_t shift = 0x80;
-
-for(i=0; i<size; i++)
-{
-  if( *(buffer+i) != 0 )
-    {
-         for(j=8; j>0; j--)
-            {
-               if((shift & *(buffer+i)) != 0)
-                 {
-                    sum++;
-                 }
-              
-               
-                shift = (shift >> 1);  
-            }
-    
-          shift = 0x80;     // I forgot to reset this shift register
-    }
-}
-printf("total number of set bits : %d\n", sum);
+  
+  while(*str != '\0')
+      {
+         putchar(*str);
+         *str++;
+      }
 
 }
+
+
