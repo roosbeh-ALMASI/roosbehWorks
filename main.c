3d578f186c0a2ad46869922e935da01c6c563c4e
Roosbeh Almasi <roosbeh@outlook.com>
2017-03-15 18:26:09 +0000

    I am in the middle of string game with <stdarg.h> and such, so much fun

--- main.c
+++ main.c
@@ -1,35 +1,78 @@
-/*      a bit of fun with function, pointer, shift register and array   */
+/*      a bit of fun with string formating   */
+/*   
 
+#include <stdarg.h>   // this have the following mcros and type
+
+va_list       <-- this is a type
+
+type  va_arg()   
+void  va_start()  
+void  va_end()
+void  va_copy() 
+
+ */
 
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 
-void roosbeh_puts(const char *str);
-
+void roosbeh_format_Str(const char *str);
+void testArgs(char x, ...);
 int main()
 {
 
 
 
-roosbeh_puts("this is going to be awsome\n");
+testArgs('A', 3, 'V', 'D', 'L');
 
+roosbeh_format_Str("the value is : %d and located at : and %f is at and %c is at\n");
 
 return 0;
 
 }
 
-void roosbeh_puts(const char *str)
-{
-  
-  while(*str != '\0')
-      {
-         putchar(*str);
-         *str++;
-      }
 
+
+void roosbeh_format_Str(const char *str)
+{
+  while(*str != '\0')
+       {
+         if(*str == '%')
+           {
+             *str++;
+             switch(*str)
+             {
+               case 'd':
+                  printf("this is a int\n");
+               break;
+               
+               case 'f':
+                 printf("this is a float\n");
+               break;
+
+              case 'c':
+                 printf("this is a character\n");
+              break;
+
+             } 
+           }
+       *str++;         
+       }
+}
+
+
+
+void testArgs(char x, ...)
+{
+
+  va_list lili;          //must be declared
+
+ va_start(lili, x);
+s
+  printf("it is quiet\n");
 }
 
 
